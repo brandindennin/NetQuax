edb11f43e0c336a7956407d4867b5549cf7bda74
Andrew Ferguson <andrewferguson343@gmail.com>
2019-03-25 22:25:33 -0400

    wrong data seed file :(

--- AdditonalDevFiles/NetQuax data seed.sql
+++ AdditonalDevFiles/NetQuax data seed.sql
@@ -1,2 +1,8 @@
-
+use NetQuax
 INSERT INTO ADDRESSES VALUES ('4 Blueberry Lane', 'Line 2', 'Danielson', 'Connecticut', 06239, 1)
+INSERT INTO USERS VALUES ('Andrew', 'Andrew', 1, 1, 1, 1)
+INSERT INTO CREDITCARD VALUES ('1234-1234-1234-1234', GETDATE(), 'VISA', 1, 1)
+INSERT INTO MOVIES VALUES ('Raiders of the Lost Ark', 'Harrison Ford is a badass','Steven Spielberg', 'Adventure', 'Harrison Ford', 1981, 4, 'HD', 12.50, 1)
+INSERT INTO USERTYPES VALUES ('Little Duckling', 2)
+INSERT INTO USERTYPES VALUES ('Full-Grown Duck', 5)
+INSERT INTO USERTYPES VALUES ('Mother Duck', 10)

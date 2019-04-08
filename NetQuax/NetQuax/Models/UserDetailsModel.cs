419cb32fd9a5460146871838bea9c1747c35b86b
Andrew Ferguson <andrew@p3tech.biz>
2019-04-08 17:26:52 -0400

    backend work

---
+++ NetQuax/NetQuax/Models/UserDetailsModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetQuax.Models
+{
+  public class UserDetailsModel
+  {
+    private int _userId;
+    private NetQuax.Entities.User _user;
+
+    public UserDetailsModel(int _userId)
+    {
+      _userId = _userId;
+      _user = null;
+    }
+
+    public NetQuax.Entities.User User
+    {
+      get
+      {
+        if(_user== null && _userId > 0)
+        {
+          _user = new NetQuax.Entities.User(_userId);
+        }
+        return _user;
+      }
+    }
+  }
+}
